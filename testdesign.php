4993c32ee074fab04a97d8f9c217795922b01472
James Lopez <tenjikrym@gmail.com>
2018-09-30 15:06:05 -0400

    Test Design

---
+++ testdesign.php
@@ -0,0 +1,38 @@
+<!doctype HTML>
+<html>
+	<head>
+		<title>Social Media Prototype Testing</title>
+		<link rel="stylesheet" type="text/css" href="test.css">
+		<link href="https://fonts.googleapis.com/css?family=PT+Sans+Narrow" rel="stylesheet">
+	</head>
+	<body>
+		<div class="header">
+			<div id="wrapper">
+				<div id="logo">
+					<t> Social Media </t>
+				</div>
+				<div id="menu">
+					<a href="#" />Home</a>
+					<a href="#" />Sign Up</a>
+					<a href="#" />Sign In</a>
+				</div>
+			</div>
+		</div>	
+		
+		<table align="center">
+			<tr>
+				<td>
+					<h2 align="center"> Join Now! </h2>
+					<form action="#" method="POST">
+						<input type="text" name="fname" size="25" placeholder="First Name" /> <br /><br />
+						<input type="text" name="lname" size="25" placeholder="Last Name" /> <br /><br />
+						<input type="text" name="username" size="25" placeholder="Username" /> <br /><br />
+						<input type="text" name="email" size="25" placeholder="Email Address" /> <br /><br />
+						<input type="text" name="password" size="25" placeholder="Password" /> <br /><br />
+						<input type="submit" name="submit" value="Sign Up"/>
+					</form>
+				</td>
+			</tr>
+		</table>
+	</body>
+</html>
