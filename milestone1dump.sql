33594e670d7478b3a1fd9d07d8dbf82af6214a0e
AndrewMayes <andrewmayes757@gmail.com>
2018-09-30 01:08:34 -0400

    Creating database with Users table

---
+++ milestone1dump.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1deb2ubuntu2.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Sep 30, 2018 at 12:43 AM
+-- Server version: 5.7.23-0ubuntu0.16.04.1
+-- PHP Version: 7.0.32-0ubuntu0.16.04.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cs418`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(4) UNSIGNED ZEROFILL NOT NULL,
+  `fname` varchar(32) NOT NULL,
+  `lname` varchar(32) NOT NULL,
+  `password` varchar(60) NOT NULL,
+  `email` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `fname`, `lname`, `password`, `email`) VALUES
+(0001, 'Tow', 'Mater', '@mater', 'mater@rsprings.gov'),
+(0002, 'Sally', 'Carrera', '@sally', 'porsche@rsprings.gov'),
+(0003, 'Doc', 'Hudson', '@doc', 'hornet@rsprings.gov'),
+(0004, 'Finn', 'McMissile', '@mcmissile', 'topsecret@agent.org'),
+(0005, 'Lightning', 'McQueen', '@mcqueen', 'kachow@rusteze.com'),
+(0006, 'Chick', 'Hicks', '@chick', 'chinga@cars.com');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(4) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
