e59f8c8a841f99e0abe1c780e10e58ae08af5b9d
JCFLopez <43054821+JCFLopez@users.noreply.github.com>
2018-10-01 14:35:35 -0400

    Adding testsignin page

--- testdesign/testsignin.php
+++ testdesign/testsignin.php
@@ -21,14 +21,11 @@
 		<table>
 			<tr>
 				<td>
-					<h2 align="center"> Join Now! </h2>
+					<h2 align="center"> Sign in </h2>
 					<form action="#" method="POST">
-						<input type="text" name="fname" size="25" placeholder="First Name" /> <br /><br />
-						<input type="text" name="lname" size="25" placeholder="Last Name" /> <br /><br />
 						<input type="text" name="username" size="25" placeholder="Username" /> <br /><br />
-						<input type="text" name="email" size="25" placeholder="Email Address" /> <br /><br />
 						<input type="text" name="password" size="25" placeholder="Password" /> <br /><br />
-						<input type="submit" name="submit" value="Sign Up"/>
+						<input type="submit" name="submit" value="Sign In"/>
 					</form>
 				</td>
 			</tr>
