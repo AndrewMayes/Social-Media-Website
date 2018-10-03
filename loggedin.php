12b91e01e58060a5f46bd1ce97696e3f00a0ccfe
JCFLopez <43054821+JCFLopez@users.noreply.github.com>
2018-10-02 20:29:52 -0400

    Fixing Log out

--- loggedin.php
+++ loggedin.php
@@ -15,8 +15,7 @@
 					<t> :Social Media:</t>
 				</div>
 				<div id="menu">
-					<a href="signup.php" />Sign Up</a>
-					<a href="index.php" />Log In</a>
+					<a href="loggedout.php" />Log Out</a>
 				</div>
 			</div>
 		</div>	
