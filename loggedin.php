94646e52102e4e688f758fcd417d6f6586476714
JCFLopez <43054821+JCFLopez@users.noreply.github.com>
2018-10-04 15:04:59 -0400

    Fixing loggedin.php

--- loggedin.php
+++ loggedin.php
@@ -1,6 +1,9 @@
 <?php
+	include ('connection.php');
+
 	session_start();
 ?>
+
 <!doctype HTML>
 <html>
 	<head>
@@ -20,19 +23,25 @@
 			</div>
 		</div>	
 		
-		<?php
-			echo "<h1 class= " . "notfound" . ">Congratulations. You logged in!</h1><br /> <br />";
-		?>
-		<table>
-			<tr>
-				<td>
-					<form action="#" method="POST">
-						<input type="text" name="post" size="25" placeholder="Post Your Status..." /> <br />
-						<input type="submit" name="submit" value="Post"/>
-					</form>
-				</td>
-			</tr>		
-		</table>
+		<ul>
+			<li><a href="#">Account</a></li>
+			<li><a href="#">Group</a>
+				<ul class='dropdown1'>
+					<li> <a href="#">Global</a></li>
+					<li> <a href="#">League of Legends</a></li>
+					<li> <a href="#">Dota 2</a></li>
+					<li> <a href="#">CS:GO</a></li>
+				</ul>
+			</li>
+		</ul>
+		
+		<form action="loggedin.php" method="POST">
+			<div class="posting">
+				<input id="messeging" type="text" name="message" value="" placeholder="Post Your Status...">
+				<input id="msg_submit" type="submit" name="submit" value="Post!">
+			</div>
+		</form> 
+		
 		<div class="footer">
 				<p>":Social Media:": A CS418 Project</p><br/>
 				<p>Created by: Andrew Mayes and James Lopez</p>
