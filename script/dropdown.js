e07821231124c555f0bcd2b2a43418e23d3c921b
JCFLopez <43054821+JCFLopez@users.noreply.github.com>
2018-10-15 20:55:14 -0400

    Update script/dropdown.js

--- script/dropdown.js
+++ script/dropdown.js
@@ -3,31 +3,28 @@ References: https://www.sitepoint.com/community/t/how-to-keep-accordion-drop-dow
 */
 
 $(document).ready( function() {
-
+    
+    //Initializing #submenu
     $('#submenu ul').each( function() {
-        var currentURI = window.location.href; //the current page
-        var links = $('a', this); //each links
-        var collapse = true; //boolean value of collapse
-        
-        //Initializing #submenu
+        var url = $('a', this); //each links
+        var hide = true; //boolean value of hide variable
+
         //Walks through all the sub-item of #submenu and then its links
-        for (var i = 0; i < links.size(); i++) {
-            var elem = links.eq(i); //retrieving the element of each link
-            var href = elem.attr('href'); //retrieving the the value of href 
-            var hrefLength = href.length; //the length of each href
-            var compareTo = currentURI.substr(-1*hrefLength); //extracts the string of the current page
+        for (var i = 0; i < url.size(); i++) {
+            var href = (url.eq(i)).attr('href'); //retrieving the the value of attribute href 
+            var href_comp = (window.location.href).substr(-(href.length)); //extracts the string of the current page
             
             //If that href equals to the current page
             //then the dropdown/sub-item will remain open
-            if (href == compareTo) {
-                collapse = false;
+            if (href == href_comp) {
+                hide = false;
                 break;
             }
         };
 
         //If they do not equal 
         //then the dropdown/sub-item will hide all the links
-        if (collapse) {
+        if (hide) {
             $(this).hide();
         }
     });
@@ -35,12 +32,9 @@ $(document).ready( function() {
     //Handles events in dropdown
     $("#submenu").delegate('span', 'click', function() {
         var ul = $(this).next('ul');
-
         if (ul.is(':visible')) {
             ul.slideUp(500);
-        }
-        
-        else {
+        } else {
             $('#submenu ul').not(ul).slideUp(500);
             ul.slideDown(500);
         }
