0e5e6fee71f92a7a372edfb147dadb508375b5fb
AndrewMayes <andrewmayes757@gmail.com>
2018-10-22 22:43:22 -0400

    updating the database

--- milestone2dump.sql
+++ milestone2dump.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 15, 2018 at 06:40 PM
+-- Generation Time: Oct 22, 2018 at 10:42 PM
 -- Server version: 5.7.23-0ubuntu0.16.04.1
 -- PHP Version: 7.0.32-0ubuntu0.16.04.1
 
@@ -63,6 +63,7 @@ CREATE TABLE `group_users` (
 INSERT INTO `group_users` (`user_id`, `group_id`) VALUES
 (1, 1),
 (1, 2),
+(1, 3),
 (1, 4),
 (2, 1),
 (2, 3),
@@ -70,10 +71,7 @@ INSERT INTO `group_users` (`user_id`, `group_id`) VALUES
 (3, 2),
 (3, 4),
 (4, 1),
-(5, 1),
-(6, 1),
-(6, 3),
-(6, 4);
+(5, 1);
 
 -- --------------------------------------------------------
 
@@ -86,25 +84,22 @@ CREATE TABLE `messages` (
   `msg_id` int(10) UNSIGNED NOT NULL,
   `user_id` int(10) UNSIGNED NOT NULL,
   `msg` varchar(280) NOT NULL,
-  `post_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `group_id` int(10) UNSIGNED NOT NULL
+  `post_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `group_id` int(10) UNSIGNED NOT NULL,
+  `likes` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `messages`
+-- Table structure for table `messages_likes`
 --
 
-INSERT INTO `messages` (`msg_id`, `user_id`, `msg`, `post_time`, `group_id`) VALUES
-(1, 5, 'Hello. This is my first post!', '2018-10-15 22:13:36', 1),
-(2, 1, '<b>hello there friends</b>', '2018-10-15 22:15:22', 1),
-(3, 1, 'Is this the home group?', '2018-10-15 22:17:05', 1),
-(4, 2, 'Hey', '2018-10-15 22:18:36', 1),
-(5, 5, 'I hate racing games', '2018-10-15 22:20:49', 2),
-(6, 4, 'I can\'t wait for basketball season to start', '2018-10-15 22:26:53', 3),
-(7, 4, 'Are there any good spy video games?', '2018-10-15 22:28:45', 2),
-(8, 4, 'howdy ', '2018-10-15 22:29:14', 1),
-(9, 3, 'testing testing', '2018-10-15 22:29:50', 1),
-(10, 5, 'kachow', '2018-10-15 22:30:55', 1);
+DROP TABLE IF EXISTS `messages_likes`;
+CREATE TABLE `messages_likes` (
+  `msg_id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -143,8 +138,7 @@ INSERT INTO `users` (`id`, `fname`, `lname`, `password`, `email`) VALUES
 (2, 'Sally', 'Carrera', '@sally', 'porsche@rsprings.gov'),
 (3, 'Doc', 'Hudson', '@doc', 'hornet@rsprings.gov'),
 (4, 'Finn', 'McMissile', '@mcmissile', 'topsecret@agent.org'),
-(5, 'Lightning', 'McQueen', '@mcqueen', 'kachow@rusteze.com'),
-(6, 'Chick', 'Hicks', '@chick', 'chinga@cars.com');
+(5, 'Lightning', 'McQueen', '@mcqueen', 'kachow@rusteze.com');
 
 --
 -- Indexes for dumped tables
@@ -173,6 +167,14 @@ ALTER TABLE `messages`
   ADD KEY `group_id` (`group_id`);
 
 --
+-- Indexes for table `messages_likes`
+--
+ALTER TABLE `messages_likes`
+  ADD PRIMARY KEY (`msg_id`,`user_id`),
+  ADD KEY `msg_id` (`msg_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
 -- Indexes for table `sub_groups`
 --
 ALTER TABLE `sub_groups`
@@ -198,7 +200,7 @@ ALTER TABLE `groups`
 -- AUTO_INCREMENT for table `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `msg_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `msg_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=60;
 --
 -- AUTO_INCREMENT for table `sub_groups`
 --
@@ -226,6 +228,13 @@ ALTER TABLE `group_users`
 ALTER TABLE `messages`
   ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
   ADD CONSTRAINT `messages_ibfk_2` FOREIGN KEY (`group_id`) REFERENCES `groups` (`group_id`);
+
+--
+-- Constraints for table `messages_likes`
+--
+ALTER TABLE `messages_likes`
+  ADD CONSTRAINT `messages_likes_ibfk_1` FOREIGN KEY (`msg_id`) REFERENCES `messages` (`msg_id`),
+  ADD CONSTRAINT `messages_likes_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
 --
 -- Constraints for table `sub_groups`
